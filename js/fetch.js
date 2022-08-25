244d542fc352a61287d73e25647a4791b3b105d4
Mariano Coria <marcoria1994@gmail.com>
2022-08-24 21:30:54 -0300

    modificando url

--- js/fetch.js
+++ js/fetch.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //FETCH 
 
-fetch('../data/productos.json')
+fetch('/data/productos.json')
     .then(res => res.json())
     .then(productos => {
         productos.forEach((producto) => {
